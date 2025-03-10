c2c1b568189abaaa6716144768a48dfa290ddbfc
David12307 <davidmarisca09@gmail.scom>
2025-03-10 22:57:54 +0200

    Managed user limits (limiting no. of req)

--- middleware/validation.middleware.js
+++ middleware/validation.middleware.js
@@ -52,6 +52,33 @@ const updateRequestsCounter = async (apiKey) => {
     
 }
 
+const checkAndResetUsage = async (apiKey) => {
+    const { data, error } = await supabase
+        .from("api_keys")
+        .select("requests_made, last_reset")
+        .eq('key', apiKey)
+        .single();
+
+    if (error || !data) return { error: "Invalid API key" };
+
+    const { requests_made, last_reset } = data;
+    const now = new Date();
+    const lastResetTime = new Date(last_reset);
+
+    // Check if 24 hours have passed
+    if (now - lastResetTime > 24 * 60 * 60 * 1000) {
+        // Reset requests_made and update last_Reset
+        console.log("Time exceeded.");
+        await supabase
+            .from("api_keys")
+            .update({ requests_made: 0, last_reset: now })
+            .eq('key', apiKey)
+        return { requests_made: 0 };
+    }
+
+    return { requests_made };
+}
+
 export const validateAPIKey = async (req, res, next) => {
     const apiKey = req.headers['x-api-key'];
     if (!apiKey) return res.status(401).json({ error: "API Key required." });
@@ -59,13 +86,16 @@ export const validateAPIKey = async (req, res, next) => {
     const apiData = await getApiData(apiKey);
     if (!apiData) return res.status(403).json({error: "Invalid API key."});
 
-    // If user exceeded his daily limit (depends on the plan) prevent the request
-    if (apiData.requests_made >= planLimits[apiData.pricing_plan].maxRequests)
+    // Check requests made and make sure it stays in it's specific limit
+    const { requests_made, error } = await checkAndResetUsage(apiKey);
+    if (error) return res.status(403).json({error});
+    
+    if (requests_made >= planLimits[apiData.pricing_plan].maxRequests)
         return res.status(429).json({error: "Request limit exceeded."});
 
     // Increment requests_made field
-    const {error} = await updateRequestsCounter(apiKey);
-    if (error) return res.status(401).json({error: "Something went wrong... try again later."});
+    const { error: updateError } = await updateRequestsCounter(apiKey);
+    if (updateError) return res.status(401).json({error: "Something went wrong... try again later."});
 
     next();
 }
